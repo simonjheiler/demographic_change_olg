53e5b93689b81b26cb5f0788824bf7455d4eb418
Simon Heiler <simon.julian.heiler@gmail.com>
2020-01-25 16:43:47 +0100

    Port some data.

---
+++ src/analysis/auxiliary.py
@@ -0,0 +1,136 @@
+import numpy as np
+
+
+def gini(pop, val, makeplot=False):
+    """ GINI computes the Gini coefficient and the Lorentz curve.
+
+    Usage:
+        gini = gini(pop, val)
+        gini, lorentz_rel = gini(pop, val)
+        gini, lorentz_rel, lorantz_abs = gini(pop, val)
+        ... = gini(pop, val, makeplot)
+
+    Args:
+        pop (np.array): population sizes of the different classes
+        val (np.array): measurement variable (e.g. income per capita) in the
+            diffrerent classes.
+        makeplot (boolean): indicator whether a figure of the Lorentz curve
+            should be produced or not. Default is false.
+
+    Outputs:
+        gini (float): Gini coefficient
+        lorentz_rel (np.array): Lorentz curve (relative): two-column array, with
+            the left column representing cumulative population shares of the
+            different classes, sorted according to val, and the right column
+            representing the cumulative value share that belongs to the
+            population up to the given class. The Lorentz curve is a scatter
+            plot of the left vs the right column.
+        lorentz_abs (np.array): Lorentz curve (absolute): Same as lorentz_rel,
+            except that the components are not normalized to range in the unit
+            interval. Thus, the left column of a is the absolute cumulative
+            population sizes of the classes, and the right column is the
+            absolute cumulative value of all classes up to the given one.
+
+    Example:
+        x = rand(100,1);
+        y = rand(100,1);
+        gini(x,y,true);  # random populations with random incomes figure;
+        gini(x,ones(100,1),true);  # perfect equality
+
+    Explanation: The vectors pop and val must be equally long and must contain
+        only positive values (zeros are also acceptable). A typical application
+        would be that pop represents population sizes of some subgroups (e.g.
+        different countries or states), and val represents the income per
+        capita in this different subgroups. The Gini coefficient is a measure
+        of how unequally income is distributed between these classes. A
+        coefficient of zero means that all subgroups have exactly the same
+        income per capital, so there is no dispesion of income; A very large
+        coefficient would result if all the income accrues only to one subgroup
+        and all the remaining groups have zero income. In the limit, when the
+        total population size approaches infinity, but all the income accrues
+        only to one individual, the Gini coefficient approaches unity.
+
+        The Lorenz curve is a graphical representation of the distribution. If
+        (x,y) is a point on the Lorenz curve, then the poorest x-share of the
+        population has the y-share of total income. By definition, (0,0) and
+        (1,1) are points on the Lorentz curve (the poorest 0% have 0% of total
+        income, and the poorest 100% [ie, everyone] have 100% of total income).
+        Equal distribution implies that the Lorentz curve is the 45 degree line.
+        Any inequality manifests itself as deviation of the Lorentz curve from
+        the 45 degree line. By construction, the Lorenz curve is weakly convex
+        and increasing.
+
+        The two concepts are related as follows: The Gini coefficient is twice
+        the area between the 45 degree line and the Lorentz curve.
+
+    Author : Yvan Lengwiler
+    Release: $1.0$
+    Date   : $2010-06-27$
+
+    """
+    # check arguments
+    assert np.prod(pop.shape) == np.prod(
+        val.shape
+    ), "gini expects two equally long vectors (#d ~= #d).".format(
+        np.prod(pop.shape), np.prod(val.shape)
+    )
+
+    pop = np.pad(pop, (1, 0), "constant", constant_values=(0, 0))  # pre-append a zero
+    val = np.pad(pop, (1, 0), "constant", constant_values=(0, 0))
+
+    isok = ~np.isnan([pop, val])  # filter out NaNs
+    if sum(isok) < 2:
+        print("gini:lacking_data", "not enough data")
+        gini = np.nan
+        lorentz_rel = np.nan(1, 4)
+        lorentz_abs = np.nan(1, 4)
+        return
+
+    pop = pop[isok]
+    val = val[isok]
+
+    assert np.all(pop >= 0) and np.all(val >= 0)
+    #     "gini expects nonnegative vectors (neg elements in pop = #d, in val = #d).".format(
+    #     sum(pop < 0), sum(val < 0)
+    # )
+
+    # process input
+    z = val * pop
+    [_, ord] = np.sort(val)
+    pop = pop[ord]
+    z = z[ord]
+    pop = np.cumsum(pop)
+    z = np.cumsum(z)
+    relpop = pop / pop[-1]
+    relz = z / z[-1]
+
+    # Gini coefficient
+
+    # We compute the area below the Lorentz curve. We do this by
+    # computing the average of the left and right Riemann-like sums.
+    # (I say Riemann-'like' because we evaluate not on a uniform grid, but
+    # on the points given by the pop data).
+    #
+    # These are the two Rieman-like sums:
+    #    leftsum  = sum(relz(1:end-1) .* diff(relpop));
+    #    rightsum = sum(relz(2:end)   .* diff(relpop));
+    # The Gini coefficient is one minus twice the average of leftsum and
+    # rightsum. We can put all of this into one line.
+    gini = 1 - sum((relz[:-1] + relz[2:]) * np.diff(relpop))
+
+    # Lorentz curve
+    lorentz_rel = [relpop, relz]
+    lorentz_abs = [pop, z]
+    if makeplot:  # ... plot it?
+        # area(relpop, relz, 'FaceColor', [0.5, 0.5, 1.0])  # the Lorentz curve
+        # plot([0, 1], [0, 1], '--k')  # 45 degree line
+        # axis tight  # ranges of abscissa and ordinate are by definition exactly [0,1]
+        # axis square  # both axes should be equally long
+        # set(gca, 'XTick', get(gca, 'YTick'))  # ensure equal ticking
+        # set(gca, 'Layer', 'top')  # grid above the shaded area
+        # title(['\bfGini coefficient = ', str(g)])
+        # xlabel('share of population')
+        # ylabel('share of value')
+        pass
+
+    return gini, lorentz_rel, lorentz_abs
--- src/model_specs/setup.py
+++ src/model_specs/setup.py
@@ -1,28 +1,37 @@
+import json
+
 import numpy as np
 import pandas as pd
 
 from bld.project_paths import project_paths_join as ppj
+from src.analysis.auxiliary import gini
 
 num_income_states = 2
 reform = 1
-J = 100
-n = 2
-nk = 2
-tR = 25
-tW = 30
-N = 1
+J = 66  # life-span
+JR = 46  # age of retirement
+tR = J - JR + 1  # length of retirement
+tW = JR - 1  # length of working life
+n = 0.011  # Population growth
+N = 2  # number of shock realizations
 
-productivity_rates = pd.read_csv(ppj("IN_DATA", "ef.csv"))
-survival_rates = pd.read_csv(ppj("IN_DATA", "sr.csv"))
+eff = pd.read_csv(ppj("IN_DATA", "ef.csv"))
+sr = pd.read_csv(ppj("IN_DATA", "sr.csv"))
 
+params = json.load("setup.json")
 
-print(productivity_rates)
-print(survival_rates)
+alpha = params["alpha"]
+beta = params["beta"]
+sigma = params["sigma"]
+delta = params["delta"]
+
+print(eff)
+print(sr)
 
 # Distribution of newborns over shocks
-income_distribution_init = [0.2037, 0.7963]
+z_init = [0.2037, 0.7963]
 
-income_state_transition = pd.DataFrame(
+tran = pd.DataFrame(
     data=[[0.9261, 1.0 - 0.9261], [1.0 - 0.9811, 0.9811]],
     index=list(range(num_income_states)),
     columns=list(range(num_income_states)),
@@ -35,6 +44,15 @@ for j in range(2, J):
 
 # Normalized measure of each generation (sum up to 1)
 mass = mass / sum(mass)
+
+
+maxkap = 30  # maximum value of capital grid
+minkap = 0.001  # minimum value of capital grid
+nk = 1000  # number of grid points
+inckap = (maxkap - minkap) / (nk - 1)  # distance between points
+aux = range(nk)
+kap = minkap + inckap * (aux - 1)
+
 
 # Capital grid
 capital_min = 0.001  # minimum value of capital grid
@@ -103,3 +121,277 @@ kapWopt = np.ones(N, nk, tW)  # optimal savings of workers
 labopt = np.ones(N, nk, tW)  # optimal labor supply
 
 neg = -1e10  # very small number
+
+
+################################################################
+# Loop over capital, labor and pension benefits
+################################################################
+while (q < nq) and ((abs(k1 - k0) > tolk) or (abs(l1 - l0) > tollab)):
+
+    q = q + 1
+
+    print(f"Iteration {q} out of {nq}")
+
+    # Prices
+    r0 = alpha * (k0 ** (alpha - 1)) * (l0 ** (1 - alpha)) - delta
+    w0 = (1 - alpha) * (k0 ** (alpha)) * (l0 ** (-alpha))
+
+    # Pension benefit
+    b = tau * w0 * l0 / np.sum(mass[JR:])
+
+    ############################################################
+    # BACKWARD INDUCTION
+    ############################################################
+
+    # Retired households
+
+    # Last period utility
+    consumption = (1 + r0) * kap + b  # last period consumption (vector!)
+    flow_utility = (consumption ** ((1 - sigma) * gamma)) / (
+        1 - sigma
+    )  # last period utility (vector!)
+    vR[:, tR] = flow_utility  # last period indirect utility (vector!)
+
+    for age in range(tR, -1, -1, -1):  # age
+        for assets_this_period_idx in range(nk):  # assets today
+
+            # Initialize right-hand side of Bellman equation
+            vmin = neg
+            assets_next_period_idx = 0
+            # More efficient is to use:
+            # l = min(kapRopt(max(j-1,1),i),nk-1)-1;
+
+            # Loop over all k's in the capital grid to find the value,
+            # which gives max of the right-hand side of Bellman equation
+
+            while assets_next_period_idx < nk:  # assets tomorrow
+                assets_next_period_idx += 1
+                assets_this_period = kap[
+                    assets_this_period_idx
+                ]  # current asset holdings
+                assets_next_period = kap[
+                    assets_next_period_idx
+                ]  # future asset holdings
+
+                # Instantaneous utility
+                consumption = (1 + r0) * assets_this_period + b - assets_next_period
+
+                if consumption <= 0:
+                    flow_utility = neg
+                    assets_next_period_idx = nk
+                else:
+                    flow_utility = (consumption ** ((1 - sigma) * gamma)) / (1 - sigma)
+
+                # Right-hand side of Bellman equation
+                v0 = flow_utility + beta * vR[assets_next_period_idx, age + 1]
+
+                # Store indirect utility and optimal saving
+                if v0 > vmin:
+                    vR[assets_this_period_idx, age] = v0
+                    kapRopt[assets_this_period_idx, age] = assets_next_period_idx
+                    vmin = v0
+
+    # Working households
+    for age in range(tW, -1, -1, -1):  # age
+        for e in range(N):  # productivity shock
+            for assets_this_period_idx in range(nk):  # assets today
+
+                # Initialize right-hand side of Bellman equation
+                vmin = neg
+                assets_next_period_idx = 0
+                # More efficient is to use:
+                # l=min(kapWopt(e,max(j-1,1),i),nk-1)-1;
+
+                # Loop over all k's in the capital grid to find the value,
+                # which gives max of the right-hand side of Bellman equation
+
+                while assets_next_period_idx < nk:  # assets tomorrow
+                    assets_next_period_idx += 1
+
+                    assets_this_period = kap[
+                        assets_this_period_idx
+                    ]  # current asset holdings
+                    assets_next_period = kap[
+                        assets_next_period_idx
+                    ]  # future asset holdings
+
+                    # Optimal labor supply
+                    lab = (
+                        gamma * (1 - tau) * z[e] * eff[age] * w0
+                        - (1 - gamma)
+                        * ((1 + r0) * assets_this_period - assets_next_period)
+                    ) / ((1 - tau) * w0 * z[e] * eff[age])
+
+                    # Check feasibility of labor supply
+                    if lab > 1:
+                        lab = 1
+                    elif lab < 0:
+                        lab = 0
+
+                    # Instantaneous utility
+                    consumption = (
+                        (1 + r0) * assets_this_period
+                        + (1 - tau) * w0 * z[e] * eff[age] * lab
+                        - assets_next_period
+                    )
+
+                    if consumption <= 0:
+                        flow_utility = neg
+                        assets_next_period_idx = nk
+                    else:
+                        flow_utility = (
+                            ((consumption ** gamma) * (1 - lab) ** (1 - gamma))
+                            ** (1 - sigma)
+                        ) / (1 - sigma)
+
+                    # Right-hand side of Bellman equation
+
+                    if age == tW:  # retired next period
+                        v0 = flow_utility + beta * vR[assets_next_period_idx, 1]
+                    else:
+                        v0 = flow_utility + beta * (
+                            tran[e, 1] * vW[1, assets_next_period_idx, age + 1]
+                            + tran[e, 2] * vW[2, assets_next_period_idx, age + 1]
+                        )
+
+                    # Store indirect utility, optimal saving and labor
+                    if v0 > vmin:
+                        vW[e, assets_this_period_idx, age] = v0
+                        kapWopt[e, assets_this_period_idx, age] = assets_next_period_idx
+                        labopt[e, assets_this_period_idx, age] = lab
+                        vmin = v0
+
+############################################################################
+# Aggregate capital stock and employment
+############################################################################
+
+# Initializations
+kgen = np.zeros(J, 1)  # Aggregate capital for each generation
+
+# Distribution of workers over capital and shocks for each working cohort
+gkW = np.zeros(N, nk, tW)
+
+# Newborns
+gkW[1, 1, 1] = z_init[1] * mass[1]  # Mass of high shock agents at age 1
+# (recall that agents are born with zero assets = 1st element in the asset grid!)
+gkW[2, 1, 1] = z_init[2] * mass[1]  # Mass of low shock agents at age 1
+
+# Distribution of agents over capital for each cohort (pooling together
+# both productivity shocks). This would be useful when computing total
+# capital
+gk = np.zeros(nk, J)
+gk[1, 1] = mass[1]  # Distribution of newborns over capital
+
+# Aggregate labor supply by generation
+labgen = np.zeros(tW, 1)
+
+# Distribution of retirees over capital
+gkR = np.zeros(nk, tR)
+
+############################################################################
+# Iterating over the distribution
+############################################################################
+# Workers
+for ind_age in range(tW):  # iterations over cohorts
+    for ind_k in range(nk):  # current asset holdings
+        for ind_e in range(N):  # current shock
+
+            ind_kk = kapWopt[
+                ind_e, ind_k, ind_age
+            ]  # optimal saving (as index in asset grid)
+
+            for ind_ee in range(N):  # tomorrow's shock
+
+                if ind_age < tW:
+                    gkW[ind_ee, ind_kk, ind_age + 1] += (
+                        tran[ind_e, ind_ee] * gkW[ind_e, ind_k, ind_age] / (1 + n)
+                    )
+                elif (
+                    ind_age == tW
+                ):  # need to be careful because workers transit to retirees
+                    gkR[ind_kk, 1] += (
+                        tran[ind_e, ind_ee] * gkW[ind_e, ind_k, ind_age] / (1 + n)
+                    )
+
+    # Aggregate labor by age
+    labgen[ind_age] = 0
+
+    for ind_k in range(nk):
+        for ind_e in range(N):
+            labgen[ind_age] += (
+                z[ind_e]
+                * eff[ind_age]
+                * labopt[ind_e, ind_k, ind_age]
+                * gkW[ind_e, ind_k, ind_age]
+            )
+
+    # Aggregate capital by age
+    for ind_k in range(nk):
+        if ind_age < tW:
+            gk[ind_k, ind_age + 1] = np.sum(gkW[:, ind_k, ind_age + 1])
+        else:
+            gk[ind_k, ind_age + 1] = gkR[ind_k, 1]
+
+    kgen[ind_age + 1] = kap * gk[:, ind_age + 1]
+
+
+# Retirees
+for ind_age in range(tR, -1, -1, -1):  # iterations over cohort
+
+    for ind_k in range(nk):  # current asset holdings
+        ind_kk = kapRopt[ind_k, ind_age]  # optimal saving (as index in asset grid)
+        gkR[ind_kk, ind_age + 1] = gkR[ind_kk, ind_age + 1] + gkR[ind_k, ind_age] / (
+            1 + n
+        )
+
+    # Distribution by capital and age
+    gk[:, tW + ind_age + 1] = gkR[:, ind_age + 1]
+    # Aggregate capital by age
+    kgen[tW + ind_age + 1] = kap * gk[:, tW + ind_age + 1]
+
+k1 = np.sum(kgen)
+l1 = np.sum(labgen)
+
+# Update the guess on capital and labor
+k0 = 0.95 * k0 + 0.05 * k1
+l0 = 0.95 * l0 + 0.05 * l1
+
+# Display results
+print("capital | labor | pension")
+print([k0, l0, b])
+print("deviation-capital | deviation-labor")
+print([abs(k1 - k0), abs(l1 - l0)])
+
+# Display equilibrium results
+print("k0 | l0 | w | r | b ")
+print([k0, l0, w0, r0, b])
+
+# Prices
+r0 = alpha * (k0 * (alpha - 1)) * (l0 ** (1 - alpha)) - delta
+w0 = (1 - alpha) * (k0 ** (alpha)) * (l0 ** (-alpha))
+
+# Mass of agent at upper bound of assets
+np.sum(gk[:, :])
+
+# Average hours worked
+h = np.zeros(tW, 1)
+
+for d in range(tW):
+    for ii in range(nk):
+        for jj in range(N):
+            h[d] += labopt[jj, ii, d] * gkW[jj, ii, d]
+    h[d] = h[d] / sum(sum(gkW[:, :, d]))
+
+# Gini disposable income
+incomeW = np.zeros(N, nk, tW)
+
+for d in range(tW):
+    for ii in range(nk):
+        for jj in range(N):
+            incomeW[jj, ii, d] = r0 * kap[ii] + z[jj] * eff[d] * labopt[jj, ii, d]
+
+incomeR = [r0 * kap + b] * tR
+
+pop = [gkW[:], gkR[:]]
+income = [incomeW[:], incomeR[:]]
+gini_index = gini(pop, income)
